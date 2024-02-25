a1151969650d8a39b96a7ddbd1a3f5528fc8b523
Alex Hinqo <tinocoalex003@gmail.com>
2024-02-25 01:17:40 +0100

    feature : js script to show container + fix accessiblities issues (button)

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+var ContainerInfos = document.getElementById("ContainerInfos")
+var Choix = document.getElementById("parentgridschedule")
+var Afficher = false;
 
+function ShowInfos() {
+    if (Afficher == false){
+        ContainerInfos.style.display = "block";
+        Afficher = true;
+    }
+    else {
+        ContainerInfos.style.display = "none";
+        Afficher = false;
+    }
+}
